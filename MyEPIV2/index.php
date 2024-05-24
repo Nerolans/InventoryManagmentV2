d9a5f06d1fdb8f55dfbcf03f7e4f05deafa9f70c
Guillaume Hyvert <guig.hyvert@gmail.com>
2024-05-24 14:40:04 +0200

    base tree + index

---
+++ MyEPIV2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+  <!--
+  Auteur: Guiullaume Hyvert
+  Date: beggining of 2020
+  Déscription: Redirection vers Page Accueil.php
+  -->
+
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; URL=src/php/controllers/mainController.php?view=Accueil">
+    <title>index</title>
+
+</head>
+<body>
+
+</body>
+</html>
